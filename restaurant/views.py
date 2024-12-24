6ab2ab3370d90fee6bbd03430eab3c9bceac6a25
Prasanna <socpds@gmail.com>
2024-12-24 20:01:01 +0530

    serilaizers, viewsets, APIviews, DRF

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+#from rest_framework.decorators import permission_classes
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# class BookingView(ListCreateAPIView):
+#     queryset = Booking.objects.all()
+#     serializer_class = BookingSerializer   
+
+# class SingleBookingView(RetrieveUpdateDestroyAPIView):
+#     queryset = Booking.objects.all()
+#     serializer_class = BookingSerializer        
+
+class MenuView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
